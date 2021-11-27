c97abf4874a4b723c383b9d70f3c46f1c640c97b
Rei <33325105+Eilaluth@users.noreply.github.com>
2021-11-27 17:43:59 +0700

    Update tiktok.js

--- commands/utility/tiktok.js
+++ commands/utility/tiktok.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require("discord.js");
-const fxc7Tiktok = require("fxc7-tiktok");
+const tiktok = require('tiktok-scraper');
 const isUrl = require('is-valid-http-url');
+const axios = require('axios');
 
 module.exports = {
   name: "tiktok",
@@ -10,23 +11,35 @@ module.exports = {
   description: "download tiktok video",
 
   execute(client, message, args) {
-    if (!args[0]) { return message.channel.send("Please input valid tiktok link!");} 
+    if (!args[0]) { return message.channel.send("Please input valid tiktok link!"); }
     if (isUrl(args[0])) {
-        fxc7Tiktok.tiktok(args[0])
-        .then(res => {
-            const embed = new MessageEmbed()
-            .setTitle(res.seoProps.metaParams.title)
-            .setDescription(res.seoProps.metaParams.description)
-            .addField(`**❱ DOWNLOAD**`,`**[LINK1](${res.itemInfo.itemStruct.video.playAddr})** - **[LINK2](${res.itemInfo.itemStruct.video.downloadAddr})**`, true)
-            .setThumbnail(res.itemInfo.itemStruct.video.originCover)
-            .setColor(client.config.app.color)
-            message.channel.send({ embeds: [embed] });
-        }).catch(err => {
-            console.log(err)
-            message.channel.send('Something went wrong, please try again later.')
+      tiktok.getVideoMeta(args[0]).then(async (video) => {
+        axios({
+          method: 'post',
+          url: 'https://chiyome.ninja/api/url/add',
+          headers: {
+            'Authorization': `Token ${client.config.key.chiyomeAPIKey}`,
+            'Content-Type': 'application/json'
+          },
+          data: {
+            "url": `${video.collector[0].videoUrl}`
+          }
         })
+          .then(function (responsx) {
+            const embed = new MessageEmbed();
+            embed.setTitle(`**${video.collector[0].authorMeta.nickName} (@${video.collector[0].authorMeta.name})**`)
+            embed.setDescription(`${video.collector[0].text}`)
+            embed.setThumbnail(video.collector[0].imageUrl)
+            embed.addField(`**❱ DOWNLOAD**`, `**[MP4](${responsx.data.short})**`, true);
+            embed.setColor(client.config.app.color);
+            message.channel.send({ embeds: [embed] });
+          })
+          .catch(function (errox) {
+            console.log(errox);
+          });
+      });
     } else {
-        message.channel.send('Error! The input is not a valid link! Please make sure it is a valid link.\nWhen inputting the link please add `https://`!');
+      message.channel.send('Error! The input is not a valid link! Please make sure it is a valid link.\nExample : https://www.tiktok.com/@nnaemie/video/7028491495388777755');
     }
   },
 };
