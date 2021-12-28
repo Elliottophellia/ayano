efd66b3ad2ff65b47cea0f4a591f4eb66ec27967
Rei <33325105+Eilaluth@users.noreply.github.com>
2021-12-29 04:33:50 +0700

    change api from rapidapi to libretranslate
    open souce is better, credit to https://github.com/LibreTranslate/LibreTranslate

--- commands/utility/translate.js
+++ commands/utility/translate.js
@@ -14,11 +14,9 @@ module.exports = {
     if (!args.slice(2).join('')) { return message.channel.send("Please input what you want to translate!")}
     var options = {
         method: 'POST',
-        url: 'https://deep-translate1.p.rapidapi.com/language/translate/v2',
+        url: 'https://libretranslate.de/translate',
         headers: {
-          'content-type': 'application/json',
-          'x-rapidapi-host': 'deep-translate1.p.rapidapi.com',
-          'x-rapidapi-key': client.config.key.rapidAPI
+          'Content-Type': 'application/json'
         },
         data: {q: `${args.slice(2).join(' ')}`, source: `${args[0]}`, target: `${args[1]}`}
       };
@@ -27,7 +25,7 @@ module.exports = {
       .then(function (response) {
           const embed = new MessageEmbed();
           embed.addField(`**❱ ${args[0]}**`,`${args.slice(2).join(' ')}`, true);
-          embed.addField(`**❱ ${args[1]}**`,`${response.data.data.translations.translatedText}`, true);
+          embed.addField(`**❱ ${args[1]}**`,`${response.data.translatedText}`, true);
           embed.addField('\u200b', 'Find wrong translation? please check your country code [here](http://www.lingoes.net/en/translator/langcode.htm)', false)
           embed.setColor(client.config.app.color);
           message.channel.send({ embeds: [embed] });
